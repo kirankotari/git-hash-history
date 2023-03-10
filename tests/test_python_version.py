f1156fea8a1cfc648aaec2a744b18809546db7a4
Kiran Kumar Kotari <kirankotari@live.com>
2023-03-10 00:17:17 -0500

    conf pytest

--- tests/test_python_version.py
+++ tests/test_python_version.py
@@ -6,3 +6,10 @@ def test_python_version(unittest):
     print('python version test')
     assert sys.version == '3.7.16'
 
+# content of test_sample.py
+def test_answer(cmdopt):
+    if cmdopt == "type1":
+        print("first")
+    elif cmdopt == "type2":
+        print("second")
+    assert 0  # to see what was printed
